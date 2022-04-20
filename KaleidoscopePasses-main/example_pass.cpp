2ca4a9fa64c7e398199e71f5321cce76df3eb50d
athomasx <atx2@case.edu>
2022-04-19 21:50:08 -0400

    should be done

--- KaleidoscopePasses-main/example_pass.cpp
+++ KaleidoscopePasses-main/example_pass.cpp
@@ -4,15 +4,15 @@
 #include <vector>
 #include "llvm/IR/Value.h"
 
-std::vector<double> findloopheader(ForExprAst* forloop){
-  std::vector<double> header;
-
-  NumberExprAst* step = dynamic_cast<NumberExprAST*>(forloop-> )
-
-  ForExprAst* valueright = dynamic_cast<ForExprAST*>(exp->RHS.get())
-  ForExprAst* valueright = dynamic_cast<ForExprAST*>(exp->LHS.get())
 
   
+  std::vector<double> findloopheader(ForExprAst* forloop){
+  	std::vector<double> header;
+  
+  	if(forloop){
+  		ExprAST* body1 = forloop ->Body.get();
+  
+
   
   
   
@@ -24,14 +24,14 @@ std::vector<double> findloopheader(ForExprAst* forloop){
   
   
   NumberExprAST* step = dynamic_cast<NumberExprAST*>(forloop->
+  if(step){
+  header.push_back(step->Val);
+  }
+  else{
   
+  }
   
-  
-  
-  
-  
-  
-  
+  NumberExprAST* start = dynamic_cast<NumberExprAST*>(forloop->
   	if(start){
   	header.push_back(start->Val);
   	printf(" this is 1: %f \n", start->Val);
@@ -59,27 +59,27 @@ std::vector<double> findloopheader(ForExprAst* forloop){
   for(auto const&x : TheModule->Functions){
   
   
-  FunctionAST* func = x.second.first.get();
-  ForExprAST* forloop;
-  BinaryExprAST* exp = dynamic_cast<BinaryExprAST*>(func->Body.get());
+  	FunctionAST* func = x.second.first.get();
+  	ForExprAST* forloop;
+  	BinaryExprAST* exp = dynamic_cast<BinaryExprAST*>(func->Body.get());
   
-  ForExprAST* valueright = dynamic_cast<ForExprAST*>(exp->RHS.get());
-  ForExprAST* valueleft = dynamic_cast<ForExprAST*>(exp->LHS.get());
+  	ForExprAST* valueright = dynamic_cast<ForExprAST*>(exp->RHS.get());
+  	ForExprAST* valueleft = dynamic_cast<ForExprAST*>(exp->LHS.get());
   
-  std::vector<double> header1 = findloopheader(valueleft);
-  std::vector<double> header2 = findloopheader(valueright);
+  	std::vector<double> header1 = findloopheader(valueleft);
+  	std::vector<double> header2 = findloopheader(valueright);
 
-  if(headerfor1.size() == 3 && headerfor2.size() == 3){
-    bool same = true;
-    for(int i = 0; i < 3; i++){
-      if(headerfor1.at(i) != headerfor2.at(i)){
-	 same = false;
-         break;
-      }
-    }
-    if(same){
-      printf("test");
-    }
+  	if(headerfor1.size() == 3 && headerfor2.size() == 3){
+    	bool same = true;
+    	for(int i = 0; i < 3; i++){
+    	  if(headerfor1.at(i) != headerfor2.at(i)){
+		same = false;
+     	    	break;
+      	  }
+    	}
+    	if(same){
+      		printf("test");
+    	}
   }
 
 
