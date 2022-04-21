44497e5d5e41cd0977c5d65c72126be53bf1af15
athomasx <atx2@case.edu>
2022-04-20 21:47:56 -0400

    optimized code

--- KaleidoscopePasses-main/example_pass.cpp
+++ KaleidoscopePasses-main/example_pass.cpp
@@ -12,55 +12,42 @@
   	if(loop){
   		ExprAST* body = loop->Body.get();
   		NumberExprAST* step = dynamic_cast<NumberExprAST*>(loop->Step.get());
-  		if(step){
+  		NumberExprAST* start = dynamic_cast<NumberExprAST*>(loop->Start.get());
+  		BinaryExprAST* end = dynamic_cast<BinaryExprAST*>(loop->End.get());
+  		if(step && start && end){
   			expression.push_back(step->Val);
+  			expression.push_back(start->Val);
+  			NumberExprAST* RValue = dynamic_cast<NumberExprAST*>(end->RHS.get());
+  			if(RValue){
+  				printf(" this is supposed to be n: %f \n", RValue->Val);
+  				expression.push_back(RValue->Val);
+  			}
   		}
   		else{
   
-  		}
-  
-  		NumberExprAST* start = dynamic_cast<NumberExprAST*>(loop->Start.get());
-  		if(start){
-  			expression.push_back(start->Val);
-  			printf(" this is 1: %f \n", start->Val);
-  		}else{
-  	
-  		}
-  		BinaryExprAST* end = dynamic_cast<BinaryExprAST*>(loop->End.get());
-  		if(end){
-  
-  			NumberExprAST* rvalue = dynamic_cast<NumberExprAST*>(end->RHS.get());
-  			if(rvalue){
-  				printf(" this is supposed to be n: %f \n", rvalue->Val);
-  				expression.push_back(rvalue->Val);
-  			}
   		}
   	}
   	return expression;
   }
   
   void ExamplePass(ModuleAST* TheModule) {
-  //fprintf(stderr, "This Module has %lu externs and %lu functions!\n\n",
   	for(auto const& n : TheModule->Functions){
   		FunctionAST* func = n.second.first.get();
   		ForExprAST* loop;
   		BinaryExprAST* body = dynamic_cast<BinaryExprAST*>(func->Body.get());
   
   		ForExprAST* RValue = dynamic_cast<ForExprAST*>(body->RHS.get());
-  		ForExprAST* LValue = dynamic_cast<ForExprAST*>(body->LHS.get());
-  
   		std::vector<double> exp1 = FindLoopExpression(RValue);
+  		
+  		
+  		ForExprAST* LValue = dynamic_cast<ForExprAST*>(body->LHS.get());		
   		std::vector<double> exp2 = FindLoopExpression(LValue);
 
   		if(exp1.size() == 3 && exp2.size() == 3){
-    			bool check = true;
-    			for(int i = 0; i < 3; i++){
-    	  			if(exp1.at(i) != exp2.at(i)){
-					check = false;
-					//printf("Incorrect headers: %f %f \n", exp1.at(i), exp2.at(i));
-      	  			}
-    			}
-    			if(check){
+    	  		if((exp1.at(0) != exp2.at(0)) || (exp1.at(1) != exp2.at(1)) || (exp1.at(2) != exp2.at(2))){
+					printf("Expressions not equal \n");
+      	  		}
+    			else{
       				printf("test");
     			}		
   		}	
